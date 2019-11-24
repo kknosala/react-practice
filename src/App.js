457600ceffb248493f49e0869d451a716ff72c22
kknosala <51036712+kknosala@users.noreply.github.com>
2019-11-24 09:21:33 -0700

    code upload

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Todos from "./components/Todos";
+import Header from "./components/layout/Header";
+import AddTodo from "./components/AddTodo";
+import About from "./components/pages/About";
+// import uuid from "uuid";
+import "./App.css";
+import Axios from "axios";
+
+class App extends Component {
+  state = {
+    todos: []
+  };
+
+  componentDidMount() {
+    Axios.get("https://jsonplaceholder.typicode.com/todos").then(res =>
+      this.setState({ todos: res.data })
+    );
+  }
+
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  // Delete Todo
+  delTodo = id => {
+    Axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`).then(res =>
+      this.setState({
+        todos: [...this.state.todos.filter(todo => todo.id !== id)]
+      })
+    );
+  };
+
+  addTodo = title => {
+    Axios.post("https://jsonplaceholder.typicode.com/todos", {
+      title,
+      complete: false
+    }).then(res => this.setState({ todos: [...this.state.todos, res.data] }));
+  };
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <div className="container">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
